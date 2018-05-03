4df2aa032c177a305c1cf0338a54ff48a49eceb8
danilishe <suetin.danil@gmail.com>
2018-05-03 21:20:59 +0400

    Доделан эффект "сборка"

---
+++ src/main/java/ru/isled/smartcontrol/view/DisbuildingEffectController.java
@@ -0,0 +1,153 @@
+package ru.isled.smartcontrol.view;
+
+import javafx.beans.property.IntegerProperty;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.control.*;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.List;
+import java.util.Optional;
+import java.util.ResourceBundle;
+//todo подумать, реализовать
+public class DisbuildingEffectController implements Initializable {
+    private static List<IntegerProperty> cells;
+    private static int sizeX;
+    private static int sizeY;
+    private static DisbuildingEffectController controller;
+
+    @FXML
+    private RadioButton fromRight;
+
+    @FXML
+    private Slider effectBrightness;
+
+    @FXML
+    private Label effectBrightnessLabel;
+
+    @FXML
+    private CheckBox background;
+
+    @FXML
+    private Slider backgroundBrightness;
+
+    @FXML
+    private Label backgroundBrightnessLabel;
+
+    @FXML
+    private Spinner<Integer> blockWidth;
+
+    @FXML
+    private Spinner<Integer> traceAfter;
+
+    @FXML
+    private Spinner<Integer> traceBefore;
+
+
+    Alert window;
+
+    private DisbuildingEffectController() {
+        loadDialog();
+    }
+
+    public static DisbuildingEffectController get(List<IntegerProperty> aCells, int x, int y) {
+        cells = aCells;
+        sizeX = x;
+        sizeY = y;
+
+        if (controller == null)
+            controller = new DisbuildingEffectController();
+        return controller;
+    }
+
+    @FXML
+    public void apply() {
+        Optional<ButtonType> button = window.showAndWait();
+        if (ButtonType.OK.equals(button.get())) {
+            apply(fromRight.isSelected(),
+                    (int) effectBrightness.getValue(), blockWidth.getValue(),
+                    (int) backgroundBrightness.getValue(), background.isSelected(),
+                    traceBefore.getValue(), traceAfter.getValue());
+        }
+    }
+
+    public void apply(boolean blockFallingFromRight,
+                      int bright, int blockWidth,
+                      int bgBright, boolean isBgOpaque,
+                      int tailBefore, int tailAfter) {
+
+        int firstGlareLine = 0;
+        int lastGlareLine = 0;
+        if (isBgOpaque) cells.forEach(cell -> cell.setValue(bgBright));
+        final int totalBlockWidth = tailAfter + tailBefore + blockWidth;
+
+        // наложение бегущего блика-блока
+        int blocksCountForFillingLine = (int) Math.ceil((double) sizeX / blockWidth);
+        for (int block = 1; block <= blocksCountForFillingLine && lastGlareLine < sizeY; block++) {
+            lastGlareLine = Math.min(firstGlareLine + totalBlockWidth + sizeX, sizeY);
+
+            int lastGlareIndex = Math.min(lastGlareLine * sizeX, cells.size());
+            int firstGlareIndex = Math.min(firstGlareLine * sizeX, cells.size() - 1);
+
+            List<IntegerProperty> forGlare = cells.subList(firstGlareIndex, lastGlareIndex);
+            GlareEffectController.get(forGlare, sizeX, lastGlareLine - firstGlareLine)
+                    .apply(!blockFallingFromRight, bright, blockWidth, bgBright, false,
+                            tailBefore, tailAfter);
+
+            firstGlareLine = lastGlareLine - tailAfter - (block * blockWidth);
+        }
+
+        // наложение закраски блоков
+        firstGlareLine = 0;
+        for (int block = 1; block <= blocksCountForFillingLine; block++) {
+            int filledCellsLine = firstGlareLine + (sizeX - (block * blockWidth)) + tailBefore + 1;
+
+            for (int y = filledCellsLine; y < sizeY; y++) {
+                for (int x = 0; x < block * blockWidth; x++) {
+                    try {
+                        int index = blockFallingFromRight ? y * sizeX + x : (y + 1) * sizeX - x - 1;
+                        cells.get(index).set(bright);
+                    } catch (IndexOutOfBoundsException e) {
+                        return;
+                    }
+                }
+            }
+            firstGlareLine += totalBlockWidth + sizeX - tailAfter - (block * blockWidth);
+        }
+
+    }
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+        blockWidth.setValueFactory(new SpinnerValueFactory.IntegerSpinnerValueFactory(1, 15, 1));
+        traceBefore.setValueFactory(new SpinnerValueFactory.IntegerSpinnerValueFactory(0, 15, 0));
+        traceAfter.setValueFactory(new SpinnerValueFactory.IntegerSpinnerValueFactory(0, 15, 0));
+
+        backgroundBrightness.valueProperty().addListener(((observable, oldValue, newValue) -> {
+            backgroundBrightnessLabel.setText(String.valueOf(newValue.intValue()));
+        }));
+        effectBrightness.valueProperty().addListener(((observable, oldValue, newValue) -> {
+            effectBrightnessLabel.setText(String.valueOf(newValue.intValue()));
+        }));
+    }
+
+    public void loadDialog() {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("building.fxml"));
+        loader.setController(this);
+        window = new Alert(Alert.AlertType.CONFIRMATION);
+        try {
+            window.getDialogPane().setContent(loader.load());
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        window.setTitle("Эффект \"Сборка\"");
+        window.setHeaderText("Выберите параметры эффекта");
+
+        window.getButtonTypes().setAll(ButtonType.OK, ButtonType.CANCEL);
+
+    }
+}
---
+++ src/main/java/ru/isled/smartcontrol/view/FadeInEffectController.java
@@ -0,0 +1,104 @@
+package ru.isled.smartcontrol.view;
+
+import javafx.beans.property.IntegerProperty;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.control.*;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.List;
+import java.util.Optional;
+import java.util.ResourceBundle;
+
+public class FadeInEffectController implements Initializable {
+    private static List<IntegerProperty> cells;
+    private static int sizeX;
+    private static int sizeY;
+    private static FadeInEffectController controller;
+    @FXML
+    Slider endBright;
+
+    @FXML
+    Label endBrightLabel;
+
+    @FXML
+    Slider startBright;
+
+    @FXML
+    Label startBrightLabel;
+
+    @FXML
+    CheckBox onlyEmpty;
+
+    Alert window;
+
+    private FadeInEffectController() {
+        loadDialog();
+    }
+
+    public static FadeInEffectController get(List<IntegerProperty> aCells, int x, int y) {
+        cells = aCells;
+        sizeX = x;
+        sizeY = y;
+
+        if (controller == null)
+            controller = new FadeInEffectController();
+        return controller;
+    }
+
+    @FXML
+    public void apply() {
+        Optional<ButtonType> button = window.showAndWait();
+        if (ButtonType.OK.equals(button.get())) {
+            apply((int) endBright.getValue(), (int) startBright.getValue(), onlyEmpty.isSelected());
+        }
+    }
+
+    public void apply(double endBr, double startBr, boolean onlyEmpty) {
+
+        double step = (endBr - startBr) / (sizeY - 1);
+
+
+        for (int i = 0; i < sizeY; i++, startBr += step) {
+            for (int j = 0; j < sizeX; j++) {
+                IntegerProperty integerProperty = cells.get(i * sizeX + j);
+                if (onlyEmpty) {
+                    if (integerProperty.get() == 0) integerProperty.setValue((int) startBr);
+                } else {
+                    integerProperty.setValue((int) startBr);
+                }
+            }
+        }
+    }
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+        startBright.valueProperty()
+                .addListener(((observable, oldValue, newValue) ->
+                        startBrightLabel.setText(String.valueOf(newValue.intValue()))));
+
+        endBright.valueProperty()
+                .addListener(((observable, oldValue, newValue) ->
+                        endBrightLabel.setText(String.valueOf(newValue.intValue()))));
+    }
+
+    private void loadDialog() {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("fadeIn.fxml"));
+        loader.setController(this);
+        window = new Alert(Alert.AlertType.CONFIRMATION);
+        try {
+            window.getDialogPane().setContent(loader.load());
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        window.setTitle("Эффект \"Разгорание\"");
+        window.setHeaderText("Выберите параметры эффекта");
+
+        window.getButtonTypes().setAll(ButtonType.OK, ButtonType.CANCEL);
+
+    }
+}
